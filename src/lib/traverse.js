79a54b0d84ca50c9f2060c437090de56770175f6
Nukr <nukrs.w@gmail.com>
2016-08-04 10:42:59 +0800

    add array support

--- src/lib/traverse.js
+++ src/lib/traverse.js
@@ -1,5 +1,5 @@
 function is_object (obj) {
-  return (typeof obj === 'object' && obj !== null)
+  return Object === obj.constructor
 }
 
 // TODO: no mutate input
@@ -10,15 +10,16 @@ export default function traverse (node, env = []) {
       traverse(node[key], [...env, key])
     } else {
       const env_var = [...env, key].join('_').toUpperCase()
-      let tmp = node[key]
-      if (typeof tmp === 'number') {
-        if (typeof process.env[env_var] !== 'undefined') {
-          node[key] = parseInt(process.env[env_var])
-        } else {
-          node[key] = process.env[env_var] || tmp
+      if (typeof node[key] === 'number') {
+        node[key] = parseInt(process.env[env_var]) || node[key]
+      } else if (Array.isArray(node[key])) {
+        try {
+          node[key] = process.env[env_var] && JSON.parse(process.env[env_var]) || node[key]
+        } catch (e) {
+          throw new Error(`process.env.${env_var} is invalid array error in ${process.env[env_var]}`)
         }
       } else {
-        node[key] = process.env[env_var] || tmp
+        node[key] = process.env[env_var] || node[key]
       }
     }
   })
--- test/index.js
+++ test/index.js
@@ -22,7 +22,7 @@ test('traverse', (t) => {
     }
   }
 
-  process.env.APP_PORT = 12345
+  process.env.APP_PORT = '12345'
 
   const expected = {
     app: {
@@ -73,5 +73,123 @@ test('no mutate config', (t) => {
 
   multiconfig(config)
 
-  t.notDeepEqual(config, expected)
+  t.not(config, expected)
+})
+
+test('deeper', (t) => {
+  const config = {
+    app: {
+      port: 3000
+    },
+    rethinkdb: {
+      host: 'localhost',
+      port: 28015
+    },
+    es: {
+      host: 'localhost:9200'
+    },
+    user: {
+      name: 'nukr',
+      password: '12345',
+      skills: {
+        cs: 'programming'
+      }
+    }
+  }
+
+  process.env.APP_PORT = '12345'
+  process.env.USER_SKILLS_CS = 'gg'
+
+  const expected = {
+    app: {
+      port: 12345
+    },
+    rethinkdb: {
+      host: 'localhost',
+      port: 28015
+    },
+    es: {
+      host: 'localhost:9200'
+    },
+    user: {
+      name: 'nukr',
+      password: '12345',
+      skills: {
+        cs: 'gg'
+      }
+    }
+  }
+
+  const target_config = multiconfig(config)
+
+  t.deepEqual(target_config, expected)
+})
+
+test('array', (t) => {
+  const config = {
+    app: {
+      port: 3000
+    },
+    rethinkdb: {
+      host: 'localhost',
+      port: 28015
+    },
+    es: {
+      host: 'localhost:9200'
+    },
+    user: {
+      name: 'nukr',
+      password: '12345',
+      skills: ['programming', 'cooking']
+    }
+  }
+
+  process.env.APP_PORT = '12345'
+  process.env.USER_SKILLS = '["programming", "gg"]'
+
+  const expected = {
+    app: {
+      port: 12345
+    },
+    rethinkdb: {
+      host: 'localhost',
+      port: 28015
+    },
+    es: {
+      host: 'localhost:9200'
+    },
+    user: {
+      name: 'nukr',
+      password: '12345',
+      skills: ['programming', 'gg']
+    }
+  }
+
+  const target_config = multiconfig(config)
+
+  t.deepEqual(target_config, expected)
+})
+
+test('array', (t) => {
+  const config = {
+    app: {
+      port: 3000
+    },
+    rethinkdb: {
+      host: 'localhost',
+      port: 28015
+    },
+    es: {
+      host: 'localhost:9200'
+    },
+    user: {
+      name: 'nukr',
+      password: '12345',
+      skills: ['programming', 'cooking']
+    }
+  }
+
+  // Bad array
+  process.env.USER_SKILLS = '["programming", "gg]'
+  t.throws(() => { multiconfig(config) }, 'process.env.USER_SKILLS is invalid array error in ["programming", "gg]')
 })
