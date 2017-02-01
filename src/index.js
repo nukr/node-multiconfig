4ec0e47ff463a68d4fc057b2d2415736016db417
Nukr <nukrs.w@gmail.com>
2017-02-01 18:27:25 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import traverse from './lib/traverse'
 
 export default function multiconfig (config) {
+  // Don't mutate origin config
   let target_config = JSON.parse(JSON.stringify(config))
-  traverse(target_config)
+  traverse(target_config, func)
   return target_config
+}
+
+function func (keys, v) {
+  const envVarKey = keys.join('_').toUpperCase()
+  const envVar = process.env[envVarKey]
+  const type = Array.isArray(v) && 'array' || typeof v
+  const typeResolver = {
+    number (env, v) {
+      env = Number(env)
+      return Number.isNaN(env) ? v : env
+    },
+    string (env, v) {
+      return String(env)
+    },
+    boolean (env, v) {
+      return env !== 'false'
+    },
+    array (env, v) {
+      return JSON.parse(env)
+    }
+  }
+
+  if (typeof envVar !== 'undefined') {
+    try {
+      v = typeResolver[type](envVar, v)
+    } catch (e) {
+      if (e instanceof SyntaxError) {
+        throw new Error(`process.env.${envVarKey} is invalid array error in ${process.env[envVarKey]}`)
+      } else {
+        throw e
+      }
+    }
+  }
+
+  return v
 }
--- src/lib/traverse.js
+++ src/lib/traverse.js
@@ -1,29 +1,10 @@
-function is_object (obj) {
-  return Object === obj.constructor
-}
-
-// TODO: no mutate input
-export default function traverse (node, env = []) {
-  const keys = Object.keys(node)
-  keys.forEach(key => {
-    if (is_object(node[key])) {
-      traverse(node[key], [...env, key])
+export default function traverse (o, func, key) {
+  for (let i in o) {
+    const newKey = typeof key === 'undefined' ? [i] : [...key, i]
+    if (Object.prototype.toString.call(o[i]) === '[object Object]') {
+      traverse(o[i], func, newKey)
     } else {
-      const env_var = [...env, key].join('_').toUpperCase()
-      if (typeof node[key] === 'number') {
-        node[key] = Number(process.env[env_var]) === 0 ? 0 : Number(process.env[env_var]) || node[key]
-      } else if (Array.isArray(node[key])) {
-        try {
-          node[key] = process.env[env_var] && JSON.parse(process.env[env_var]) || node[key]
-        } catch (e) {
-          throw new Error(`process.env.${env_var} is invalid array error in ${process.env[env_var]}`)
-        }
-      } else if (typeof node[key] === 'boolean') {
-        node[key] = process.env[env_var] !== 'false'
-      } else {
-        node[key] = process.env[env_var] || node[key]
-      }
+      o[i] = func(newKey, o[i])
     }
-  })
+  }
 }
-
